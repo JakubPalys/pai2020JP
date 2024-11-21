da00eac931cb43a2c586a32792744d18099c2d15
JakubPalys <161860426+JakubPalys@users.noreply.github.com>
2024-11-21 20:30:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
+require_once 'src/controllers/AppController.php';
 
-echo 'Hi there UwU 👋';
+
+$path = $_SERVER['REQUEST_URI'];
+
+var_dump($path);
+
+if ($path == '/dashboard') {
+    $controller = new AppController();
+    $controller->render('dashboard');
+}
+else {
+    echo 'Hi there students 👋';
+}
